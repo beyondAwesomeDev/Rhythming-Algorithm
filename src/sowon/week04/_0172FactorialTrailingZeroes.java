1b7d69eec41e58f61268e68b16a7b2f2d5873031
sowon <sowonkim177@gmail.com>
2021-01-20 23:55:53 +0900

    WEEK4

---
+++ src/sowon/week04/_0172FactorialTrailingZeroes.java
@@ -0,0 +1,21 @@
+package sowon.week04;
+
+public class _0172FactorialTrailingZeroes {
+  public static int trailingZeroes(int n) {
+    //https://www.youtube.com/watch?v=rrrbC6XdLBk&feature=emb_logo
+    int result = 0;
+    while( n > 0 ){
+      n = n/5;
+      result += n;
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int n = //3; //=>0
+          // 5;//=>1
+          // 0; //=>0
+          25; //=>6
+    System.out.println(trailingZeroes(n));
+  }
+}
