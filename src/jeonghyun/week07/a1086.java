76a74293fa37e756f89cb10ce4922fda065cd525
Jeong Hyun <kjh329277@hanmail.net>
2021-02-07 12:49:12 +0900

    week07 a1084~a1086

--- src/jeonghyun/week07/a1086.java
+++ src/jeonghyun/week07/a1086.java
@@ -1,9 +1,22 @@
 package jeonghyun.week07;
 
+import java.util.Scanner;
+
 public class a1086 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 그림 파일 저장용량 계산하기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double w = sc.nextInt();
+		double h = sc.nextInt();
+		double d = sc.nextInt();
+		
+		double cal = (w*h*d)/8/1024/1024;
+		System.out.printf("%.2f MB", cal);
+		
+		
 
 	}
 
