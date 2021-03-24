1711f18d4712e0dcdcd5ebc43e82fdf6e0002f4a
Jeong Hyun <kjh329277@hanmail.net>
2021-03-24 23:42:52 +0900

    week13 Quiz

---
+++ src/jeonghyun/week13/hk_SherlockandSquares.java
@@ -0,0 +1,21 @@
+package jeonghyun.week13;
+
+public class hk_SherlockandSquares {
+	
+	static int squares(int a, int b) {
+
+		
+		int a_sqrt = (int)Math.ceil(Math.sqrt((double)a));
+		int b_sqrt = (int)Math.floor(Math.sqrt((double)b));
+	
+		
+		return (b_sqrt - a_sqrt) + 1;
+		
+    }
+
+
+	public static void main(String[] args) {
+
+	}
+
+}
