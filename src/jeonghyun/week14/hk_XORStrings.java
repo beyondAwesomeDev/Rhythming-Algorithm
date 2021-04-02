855dd83e4f424d29c3c321511e87854daf5da46f
Jeong Hyun <kjh329277@hanmail.net>
2021-04-02 23:22:37 +0900

    week14 quiz

---
+++ src/jeonghyun/week14/hk_XORStrings.java
@@ -0,0 +1,25 @@
+package jeonghyun.week14;
+
+public class hk_XORStrings {
+
+	public static String stringsXOR(String s, String t) {
+        String res = new String("");
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
