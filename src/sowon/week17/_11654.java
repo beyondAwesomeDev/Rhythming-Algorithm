1119add9cc1e33ccca34b58efcbe972f36756b54
sowon <sowonkim177@gmail.com>
2021-04-25 02:04:27 +0900

    week17

---
+++ src/sowon/week17/_11654.java
@@ -0,0 +1,20 @@
+package sowon.week17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11654 {
+  public static void main(String[] args) throws IOException {
+    //memory 11484 runtime 80
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int c = br.readLine().charAt(0);
+    System.out.println(c);
+  }
+}
+/*
+input
+A
+output
+65
+ */
