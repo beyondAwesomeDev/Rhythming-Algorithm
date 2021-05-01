fbbc6d409799866bdaae6b485ccaf97b6be80581
Jeong Hyun <kjh329277@hanmail.net>
2021-05-02 08:21:38 +0900

    week17 quiz

---
+++ src/jeonghyun/week17/quiz_bj_11654.java
@@ -0,0 +1,20 @@
+package jeonghyun.week17;
+
+import java.util.Scanner;
+
+public class quiz_bj_11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		char ans = a.charAt(0);
+		
+		System.out.println((int)ans);
+		
+		
+	}
+
+}
